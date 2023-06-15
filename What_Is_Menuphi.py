3cc492f6d4fff821e9dcae018bdb95a6637b83b8
Phillip Haberman <pjhab2020@gmail.com>
2023-06-15 17:19:55 +0800

    added introduction

---
+++ What_Is_Menuphi.py
@@ -0,0 +1,38 @@
+# import libraries
+import streamlit as st
+
+# Title
+st.title('Welcome to Menuphi!')
+
+# Introduction
+st.markdown(
+    """
+    **Menuphi** was built with one goal in mind:
+    lowering your costs and maximizing profits
+    through strategic menu engineering and data-driven insights.
+    """
+)
+
+st.markdown(
+    """
+    Let **Menuphi** revolutionize your restaurant's success,
+    taking the burden of menu analysis off your hands.
+    """
+    )
+
+st.markdown(
+    """
+    Sit back, relax, and watch your profits soar!
+    """
+)
+
+# 3-Step process
+st.markdown(
+    """
+    ### To get started, follow our easy 3-step process:\n
+    1. Download the menu data template.\n
+    2. Fill it out with your menu item data.\n
+    3. Upload the file and let **Menuphi** do the rest!
+    """
+
+)
--- functions.py
+++ functions.py
@@ -21,25 +21,25 @@ def file_downloader():
     bold_format = workbook.add_format({'bold': True})
 
     # Define a format for italicized cells
-    italic_format = workbook.add_format({'italic': True})
+    # italic_format = workbook.add_format({'italic': True})
 
     # Write the column headers to the worksheet in bold
     for col, header in enumerate(headers):
         worksheet.write(0, col, header, bold_format)
 
-    # Write the example values in the second row
-    worksheet.write(1, 0, 'Burgers', italic_format)
-    worksheet.write(1, 1, 'Classic Burger', italic_format)
-    worksheet.write(1, 2, '8.99', italic_format)
-    worksheet.write(1, 3, '3.25', italic_format)
-    worksheet.write(1, 4, '40', italic_format)
+    # # Write the example values in the second row
+    # worksheet.write(1, 0, 'Burgers', italic_format)
+    # worksheet.write(1, 1, 'Classic Burger', italic_format)
+    # worksheet.write(1, 2, '8.99', italic_format)
+    # worksheet.write(1, 3, '3.25', italic_format)
+    # worksheet.write(1, 4, '40', italic_format)
 
     # Close and save the workbook
     workbook.close()
 
     # Download button
     return st.download_button(
-        label='Download the template here',
+        label='Click to Download',
         data=output.getvalue(),
         file_name='menu_data_template.xlsx',
         mime='application/vnd.ms-excel'
@@ -52,5 +52,4 @@ def file_uploader():
     if uploaded_file is not None:
         # Can be used wherever a "file-like" object is accepted:
         df = pd.read_excel(uploaded_file)
-        return df
-        # return st.dataframe(df, hide_index=True)
+        return st.dataframe(df, hide_index=True)
